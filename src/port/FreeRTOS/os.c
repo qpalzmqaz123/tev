dcadb22fe9598635ede1779368abf20bb709517f
wangqj <wangqinjun@nanchao.org>
2017-07-24 21:12:49 +0800

    add free rtos

---
+++ src/port/FreeRTOS/os.c
@@ -0,0 +1,56 @@
+#include "tev.h"
+#include <pthread.h>
+#include <sys/time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+#if TEV_CONF_ENABLE_OS == 1
+
+static xSemaphoreHandle xQueueMutex;
+static xSemaphoreHandle xSemaTxDone;
+
+void
+tev__mutex_init()
+{
+    /* Create Mutex lock */
+    xQueueMutex = xSemaphoreCreateMutex();
+    configASSERT (xQueueMutex != NULL);
+}
+
+void
+tev__mutex_lock()
+{
+    xSemaphoreTake( xQueueMutex, portMAX_DELAY );
+}
+
+void
+tev__mutex_unlock()
+{
+    xSemaphoreGive( xQueueMutex );
+}
+
+void
+tev__event_init()
+{
+    /* Queue Semaphore */ 
+    vSemaphoreCreateBinary( xSemaTxDone );
+    configASSERT ( xSemaTxDone != NULL );
+}
+
+int
+tev__event_wait(uint64_t timeout)
+{
+	int res = 0;
+
+    xSemaphoreTake(xSemaTxDone, timeout);
+
+    return res;
+}
+
+void
+tev__event_set()
+{
+    xSemaphoreGive( xSemaTxDone );
+}
+#endif
