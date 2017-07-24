5c5622fed4cbe75a72f899c5cd3a5a30d292f94c
wangqj <wangqinjun@nanchao.org>
2017-07-24 14:02:54 +0800

    update

--- src/include/tev_loop.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TEV_LOOP_H__
-#define __TEV_LOOP_H__
-
-typedef struct {
-
-} tev_loop_t;
-
-#endif
--- src/include/tev_port.h
+++ src/include/tev_port.h
@@ -1,3 +1,7 @@
+#ifndef __TEV_PORT_H__
+#define __TEV_PORT_H__
 
 uint64_t
 tev__get_time(void);
+
+#endif
