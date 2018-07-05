b79064c61e2649b043d088102cac07be59f46486
wangqj <qpalzmqaz123@gmail.com>
2018-07-05 17:30:12 +0800

    fix an error of tev_async_send

--- src/tev_async.c
+++ src/tev_async.c
@@ -19,13 +19,26 @@ tev_async_init(tev_loop_t *loop,
 int
 tev_async_send(tev_async_t *handle)
 {
+    QUEUE *q;
+    tev_handle_t *h;
+
     tev__mutex_lock(handle->loop->mutex_handle);
+
+    /* check if handle is already in active_queue */
+    QUEUE_FOREACH(q, handle->loop->active_queue) {
+        h = QUEUE_DATA(q, tev_handle_t, active_queue);
+
+        if (h == (tev_handle_t *)handle) goto exit;
+    }
 
     QUEUE_INSERT_TAIL(handle->loop->active_queue, handle->active_queue);
 
     tev__mutex_unlock(handle->loop->mutex_handle);
 
     tev__event_set(handle->loop->event_handle);
+
+exit:
+    tev__mutex_unlock(handle->loop->mutex_handle);
 
     return 0;
 }
